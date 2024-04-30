385eba556f36264d846a94ec2ca53fb8a8cf2ba6
Voroshilov Maksim <voroshilovmax90@gmail.com>
2024-04-30 14:45:01 +0300

    added state

--- src/contract.rs
+++ src/contract.rs
@@ -1,7 +1,20 @@
-pub mod query {
-    use crate::msg::ValuerResp;
+use cosmwasm_std::{DepsMut, Response, StdResult};
 
-    pub fn value() -> ValuerResp {
-        ValuerResp { value: 0 }
+use crate::state::COUNTER;
+
+pub fn instantiate(deps: DepsMut) -> StdResult<Response> {
+    COUNTER.save(deps.storage, &0)?;
+
+    Ok(Response::new())
+}
+
+pub mod query {
+    use cosmwasm_std::{Deps, StdResult};
+
+    use crate::{msg::ValuerResp, state::COUNTER};
+
+    pub fn value(deps: Deps) -> StdResult<ValuerResp> {
+        let value = COUNTER.load(deps.storage)?;
+        Ok(ValuerResp { value })
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,71 @@
 use cosmwasm_std::{
-    entry_point, to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+    entry_point, to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response,
+    StdResult,
 };
 
 mod contract;
 pub mod msg;
+mod state;
 
 #[entry_point]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
     _msg: Empty,
 ) -> StdResult<Response> {
-    Ok(Response::new())
+    contract::instantiate(deps)
 }
 
 #[entry_point]
-pub fn query(_deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
+pub fn query(deps: Deps, _env: Env, _msg: msg::QueryMsg) -> StdResult<Binary> {
     use msg::QueryMsg::*;
 
-    match msg {
-        Value {} => to_binary(&contract::query::value()),
+    match _msg {
+        Value {} => to_json_binary(&contract::query::value(deps)?),
+    }
+}
+
+#[entry_point]
+pub fn execute(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: Empty) -> StdResult<Response> {
+    Ok(Response::new())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::msg::{QueryMsg, ValuerResp};
+
+    use super::*;
+
+    use cosmwasm_std::{Addr, Empty};
+    use cw_multi_test::{App, Contract, ContractWrapper, Executor};
+
+    fn counting_contract() -> Box<dyn Contract<Empty>> {
+        let contract = ContractWrapper::new(execute, instantiate, query);
+        Box::new(contract)
+    }
+
+    #[test]
+    fn query_value() {
+        let mut app = App::default();
+
+        let contract_id = app.store_code(counting_contract());
+        let contract_addr = app
+            .instantiate_contract(
+                contract_id,
+                Addr::unchecked("sender"),
+                &QueryMsg::Value {},
+                &[],
+                "Counting contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: ValuerResp = app
+            .wrap()
+            .query_wasm_smart(contract_addr, &QueryMsg::Value {})
+            .unwrap();
+
+        assert_eq!(resp, ValuerResp { value: 0 });
     }
 }
